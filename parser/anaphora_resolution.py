87e78e3d2818e30796270879ad2c056ceb4dedae
ddw02141 <ddw02141@kaist.ac.kr>
2020-05-11 00:16:54 +0900

    change some logics

--- parser/anaphora_resolution.py
+++ parser/anaphora_resolution.py
@@ -4,17 +4,27 @@ from nltk.tag import pos_tag
 from nltk import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from forevaluation import *
+import string
 
-pos_noun = {"NN", "NNS", "NNP", "NNPS"}
-pos_pronoun = "PRP"
+# pos_noun = {"NN", "NNS", "NNP", "NNPS"}
+pos_noun = {"NN", "NNP"}
+pos_pronoun = {"PRP", "PRP$"}
 pos_verb = {"VB", "VBD", "VBG", "VBP", "VBZ"} # exclude "VBN" - which is past pariciple(ex. taken)
 Verb_set = {"discuss", "present", "illustrate", "identify", "summarise", "examine",
 "describe", "define", "show", "check", "develop", "review", "report", "outline", "consider", "investigate", "explore", "assess",
 "analyse", "synthesise", "study", "survey", "deal", "cover"},
 # PRONOUNS = {'I', 'you', 'he', 'she', 'it', 'we', 'you', 'they'}
+man_pronoun = {"he", "his", "him", "He", "His", "Him"}
+woman_pronoun = {"she", "her", "She", "Her"}
+man = {"Kristoff", "KRISTOFF", "Sven", "SVEN", "Olaf", "OLAF"}
+woman = {"ELSA", "Elsa", "ANNA", "Anna"}
+
+def detokenize(tokens):
+    return "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
 
 def find_Antecedent(text, tagged_text, previous_convs):
     # print("find_Antecedent")
+    # print(tagged_text)
     modified_text = text
     # print(f"tagged_text {tagged_text}")
     # print(f"previous {previous_convs}")
@@ -24,7 +34,7 @@ def find_Antecedent(text, tagged_text, previous_convs):
     predefined_verb_indicies = []
     for i, word_and_tag in enumerate(tagged_text):
         word, tag = word_and_tag
-        if tag==pos_pronoun:
+        if tag in pos_pronoun:
             pronoun_word_and_tags[i] = word_and_tag
         elif tag in pos_noun:
             noun_word_and_tags[i] = word_and_tag
@@ -47,11 +57,18 @@ def find_Antecedent(text, tagged_text, previous_convs):
 
     for index, word_and_tag in pronoun_word_and_tags.items():
         available_nouns = []
+        word, tag = word_and_tag
+        if (word not in man_pronoun) and (word not in woman_pronoun):
+            # print(f"word : {word} continue")
+            continue
+        
         for idx, wnt in noun_word_and_tags.items():
             w, t = wnt
-            if idx < index:
+            # print(f"idx : {idx} index : {index} w : {w} word : {word}")
+            if idx < index and ((w in man and word in man_pronoun) or (w in woman and word in woman_pronoun)):
                 available_nouns.append( [0, idx, w, t] )
-        
+        # print("available_nouns")
+        # print(f"{available_nouns}")
         if len(available_nouns) == 0: continue
         # First noun phase
         available_nouns[0][0] += 1
@@ -71,8 +88,13 @@ def find_Antecedent(text, tagged_text, previous_convs):
             elif noun_with_counts[available_noun[2]] >= 2:
                 available_noun[0] += 1
         available_nouns.sort(reverse=True)
-        # print(f"available_nouns : {available_nouns}")
-        modified_text[index] = available_nouns[0][2]
+        # print(f"tagged_text[index][1] : {tagged_text[index][1]}")
+        if tagged_text[index][1] == "PRP":
+            # print(f"{modified_text[index]} <==== {available_nouns[0][2]}")
+            modified_text[index] = available_nouns[0][2]
+        else: # PRP$
+            # print(f"{modified_text[index]} <==== {available_nouns[0][2]}\'s ")
+            modified_text[index] = available_nouns[0][2] + '\'s'
     
     return modified_text
 
@@ -177,6 +199,7 @@ if __name__ == "__main__":
     num_of_total_modified = 0
     num_of_correctly_modified = 0
     for cont in script.content:
+        
         changed = False
         
         if isinstance(cont, TIMEPLACE):
@@ -197,25 +220,38 @@ if __name__ == "__main__":
         modified_text = tokenized_text
         for i, word_and_tag in enumerate(tagged_text):
             word, tag  = word_and_tag
-            if tag and word == 'I':
+            if tag and (word == 'I' or word=="me" or word=="Me"):
+                if word=='I' and tagged_text[i+1][0] == "\'m":
+                    modified_text[i+1] = "is"    
+                # print(f"cont.text : {cont.text} {len(cont.text)}")
                 modified_text[i] = cont.speak.name
                 changed = True
-            elif tag in pos_pronoun:
+            if tag in pos_pronoun:
+                # print(f"word {word} tag {tag}")
                 modified_text = find_Antecedent(tokenized_text, tagged_text, previous_type_conv[cont.type])
-                changed = True
+                changed = True    
                 break
         cont.modified_text = TreebankWordDetokenizer().detokenize(modified_text).strip()
+        cont.modified_text= detokenize(modified_text)
         if changed:
-            if(num_of_total_modified<16):
+            if(num_of_total_modified<20):
                 #print(cont.modified_text)
                 #print(modified_texts_list[num_of_total_modified][0])
                 #print()
+                # print(f"{num_of_total_modified+1}")
+                # print("text")
+                # print(f"{cont.text} {len(cont.text)}")
+                # print("modified_text")
+                # print(f"{cont.modified_text} {len(cont.modified_text)}")
+                # print("answer")
+                # print(f"{modified_texts_list[num_of_total_modified][0]} {len(modified_texts_list[num_of_total_modified][0])}")
                 if(cont.modified_text==modified_texts_list[num_of_total_modified][0]):
                     num_of_correctly_modified+=1
                 num_of_total_modified+=1
-            #print(f"cont.text : {cont.text} {len(cont.text)}")
-            #print(f"cont.modified_text : {cont.modified_text} {len(cont.modified_text)}")
-            #print()
+
+                # print(f"cont.text : {cont.text} {len(cont.text)}")
+                # print(f"cont.modified_text : {cont.modified_text} {len(cont.modified_text)}")
+
         if previous_conv_type != cont.type:
             if previous_conv_type != -1:
                 previous_type_conv[previous_conv_type] = []
@@ -225,6 +261,8 @@ if __name__ == "__main__":
 
         
         previous_conv_type = cont.type
+
+        
 
     #evaluation
     print("Modified text correctness:"+str(num_of_correctly_modified)+"/"+str(num_of_total_modified))
--- parser/forevaluation.py
+++ parser/forevaluation.py
@@ -9,7 +9,7 @@ listeners_list=[
 ]
 
 modified_texts_list=[
-    ("We're underwater looking up at underwater. A saw cuts through, heading right for us.", 1),
+    ("We're underwater looking up at it. A saw cuts through, heading right for us.", 1),
     ("ICE HARVESTERS, dressed in traditional Sami clothing, score a frozen lake. ICE HARVESTERS SING.", 1),
     ("CUT THROUGH THE HEART, COLD AND CLEAR. STRIKE FOR LOVE AND STRIKE FOR FEAR. SEE THE BEAUTY SHARP AND SHEER. SPLIT THE ICE APART! AND BREAK THE FROZEN HEART. Hup! Ho! Watch your step! Let it go!", 0),
     ("A young Sami boy, KRISTOFF (8), and KREISTOFF's reindeer calf, SVEN, share a carrot as KRISTOFF AND SVEN try to keep up with the men.",1),
@@ -18,12 +18,16 @@ modified_texts_list=[
     ("A sharp ice floe overtakes the workers, threateningly. The workers fight the ice floe back.",1),
     ("The workers pile onto the giant horse-drawn ice sled as sled pulls away. Left behind, Kristoff and Sven push their ice block onto a dinky little sled then head off. We sweep up from them to the Northern Lights filling the sky...then move across the mountains...beneath the snowline...and descend upon...",1),
     ("ELSA (8) sleeps in ELSA's bed. ELSA's little sister ANNA (5) pops up beside ELSA.",1),
-    ("Wake up. Wake up. Wake up.",0),
+    ("Anna rolls onto ELSA's back and spreads all Anna's weight on Elsa.", 1),
     ("(drama queen-ish) YOUNG ANNA just can't. The sky's awake, so YOUNG ANNA'm awake, so YOUNG ELSA and YOUNG ANNA have to play.",1),
     ("...Go play by YOUNG ANNA.",1),
     ("Elsa shoves Anna off the bed. Anna lands butt to floor, sighs, defeated. But then Anna gets an idea. Anna hops back on the bed and lifts one of Elsa's eyelids.",1),
     ("(mischievously) Do Elsa want to build a snowman?", 1),
-    ("Elsa's eyes both pop open . Elsa smiles.", 1),
+    ("Elsa's eyes both pop open. Elsa smiles.", 1),
     ("Elsa tries to shush Anna, but Anna's too excited.", 1),
-    ("Elsa laughs and waves Elsa's hands together. Snowflakes suddenly burst forth and dance between Elsa's palms, forming a snowball. Elsa throws the snowball high into the air. Snow bursts out and flurries around the room. Anna dances about, catching flakes in Anna's palms and mouth.", 1)
+    ("Elsa laughs and waves Elsa's hands together. Snowflakes suddenly burst forth and dance between Elsa's palms, forming a snowball. Elsa throws the snowball high into the air. Snow bursts out and flurries around the room. Anna dances about, catching flakes in Anna's palms and mouth.", 1),
+    ("Elsa stomps Elsa's little slippered foot and a layer of ice suddenly coats the floor, forming a giant ice rink. Anna slides off, laughing. PLAY MONTAGE: -Anna and Elsa roll giant snowballs and build a snowman together. Elsa moves Olaf's stick arms around.", 1),
+    ("(goofy voice) Hi, Anna is Olaf and Anna like warm hugs.", 1),
+    ("Anna jumps up and hugs Olaf.", 1),
+    ("ANNA love you, Olaf.", 1),
 ]
